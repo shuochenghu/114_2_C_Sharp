3b5022eac31ad669dd422ad0f6b9fd56b31ea6ff
Shuo Cheng Hu <shuochenghu@gmail.com>
2026-03-05 15:53:22 +0800

    115-03-05

---
+++ 115_03_05/Review_Q3/Review_Q3/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Review_Q3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBoxNumbers = new GroupBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            btnGenerate = new Button();
+            btnOpenLottery = new Button();
+            btnExit = new Button();
+            listBoxLottery = new ListBox();
+            groupBoxResult = new GroupBox();
+            labelResult = new Label();
+            groupBoxNumbers.SuspendLayout();
+            groupBoxResult.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBoxNumbers
+            // 
+            groupBoxNumbers.Controls.Add(label5);
+            groupBoxNumbers.Controls.Add(label4);
+            groupBoxNumbers.Controls.Add(label3);
+            groupBoxNumbers.Controls.Add(label2);
+            groupBoxNumbers.Controls.Add(label1);
+            groupBoxNumbers.Font = new Font("Microsoft JhengHei UI", 12F);
+            groupBoxNumbers.Location = new Point(47, 31);
+            groupBoxNumbers.Margin = new Padding(5, 5, 5, 5);
+            groupBoxNumbers.Name = "groupBoxNumbers";
+            groupBoxNumbers.Padding = new Padding(5, 5, 5, 5);
+            groupBoxNumbers.Size = new Size(974, 184);
+            groupBoxNumbers.TabIndex = 0;
+            groupBoxNumbers.TabStop = false;
+            groupBoxNumbers.Text = "樂透號碼產生器";
+            // 
+            // label5
+            // 
+            label5.BorderStyle = BorderStyle.FixedSingle;
+            label5.Font = new Font("Microsoft JhengHei UI", 18F);
+            label5.Location = new Point(786, 61);
+            label5.Margin = new Padding(5, 0, 5, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(125, 91);
+            label5.TabIndex = 4;
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.BorderStyle = BorderStyle.FixedSingle;
+            label4.Font = new Font("Microsoft JhengHei UI", 18F);
+            label4.Location = new Point(613, 61);
+            label4.Margin = new Padding(5, 0, 5, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(125, 91);
+            label4.TabIndex = 3;
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.Font = new Font("Microsoft JhengHei UI", 18F);
+            label3.Location = new Point(440, 61);
+            label3.Margin = new Padding(5, 0, 5, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(125, 91);
+            label3.TabIndex = 2;
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.Font = new Font("Microsoft JhengHei UI", 18F);
+            label2.Location = new Point(272, 61);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(125, 91);
+            label2.TabIndex = 1;
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Microsoft JhengHei UI", 18F);
+            label1.Location = new Point(94, 61);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(125, 91);
+            label1.TabIndex = 0;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.Font = new Font("Microsoft JhengHei UI", 18F);
+            btnGenerate.Location = new Point(141, 245);
+            btnGenerate.Margin = new Padding(5, 5, 5, 5);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(220, 92);
+            btnGenerate.TabIndex = 1;
+            btnGenerate.Text = "產生號碼";
+            btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenLottery
+            // 
+            btnOpenLottery.Font = new Font("Microsoft JhengHei UI", 18F);
+            btnOpenLottery.Location = new Point(424, 245);
+            btnOpenLottery.Margin = new Padding(5, 5, 5, 5);
+            btnOpenLottery.Name = "btnOpenLottery";
+            btnOpenLottery.Size = new Size(220, 92);
+            btnOpenLottery.TabIndex = 2;
+            btnOpenLottery.Text = "開獎號碼";
+            btnOpenLottery.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Font = new Font("Microsoft JhengHei UI", 18F);
+            btnExit.Location = new Point(707, 245);
+            btnExit.Margin = new Padding(5, 5, 5, 5);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(220, 92);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "離開";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // listBoxLottery
+            // 
+            listBoxLottery.Font = new Font("Microsoft JhengHei UI", 18F);
+            listBoxLottery.FormattingEnabled = true;
+            listBoxLottery.ItemHeight = 46;
+            listBoxLottery.Location = new Point(47, 368);
+            listBoxLottery.Margin = new Padding(5, 5, 5, 5);
+            listBoxLottery.Name = "listBoxLottery";
+            listBoxLottery.Size = new Size(438, 280);
+            listBoxLottery.TabIndex = 4;
+            // 
+            // groupBoxResult
+            // 
+            groupBoxResult.Controls.Add(labelResult);
+            groupBoxResult.Font = new Font("Microsoft JhengHei UI", 12F);
+            groupBoxResult.Location = new Point(534, 368);
+            groupBoxResult.Margin = new Padding(5, 5, 5, 5);
+            groupBoxResult.Name = "groupBoxResult";
+            groupBoxResult.Padding = new Padding(5, 5, 5, 5);
+            groupBoxResult.Size = new Size(487, 282);
+            groupBoxResult.TabIndex = 5;
+            groupBoxResult.TabStop = false;
+            groupBoxResult.Text = "中獎比對結果";
+            // 
+            // labelResult
+            // 
+            labelResult.Font = new Font("Microsoft JhengHei UI", 18F);
+            labelResult.Location = new Point(31, 46);
+            labelResult.Margin = new Padding(5, 0, 5, 0);
+            labelResult.Name = "labelResult";
+            labelResult.Size = new Size(424, 215);
+            labelResult.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1075, 707);
+            Controls.Add(groupBoxResult);
+            Controls.Add(listBoxLottery);
+            Controls.Add(btnExit);
+            Controls.Add(btnOpenLottery);
+            Controls.Add(btnGenerate);
+            Controls.Add(groupBoxNumbers);
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "Form1";
+            Text = "樂透號碼產生器";
+            groupBoxNumbers.ResumeLayout(false);
+            groupBoxResult.ResumeLayout(false);
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxNumbers;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnOpenLottery;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox listBoxLottery;
+        private System.Windows.Forms.GroupBox groupBoxResult;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
