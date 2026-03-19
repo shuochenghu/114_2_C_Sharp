96aa62feb20b36f5d261a73745ed68644f79857f
shuochenghu <shuochenghu@gmail.com>
2026-03-19 13:07:54 +0800

    2025=03-19

--- 115_03_05/Review_Q2/Review_Q2/obj/Debug/net8.0-windows/Review_Q2.AssemblyInfo.cs
+++ 115_03_05/Review_Q2/Review_Q2/obj/Debug/net8.0-windows/Review_Q2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Review_Q2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b5022eac31ad669dd422ad0f6b9fd56b31ea6ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Review_Q2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Review_Q2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
